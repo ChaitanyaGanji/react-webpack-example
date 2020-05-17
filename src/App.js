394e25922aba65e8b45f7fead5de7dd063b3860c
chaitanya <gchaitanya193@gmail.com>
2020-05-17 19:48:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react'
 class App extends React.Component {
     render() {
         return (
-            <div>This is my First React Webpack Project</div>
+            <div>
+                This is my First React Webpack Project
+                <input />
+            </div>
         )
     }
 }
